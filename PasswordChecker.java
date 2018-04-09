d48bc7f93411e94530949433d89e5348a005ce54
Joshua Yee <yeej2@aldenv101.allegheny.edu>
2018-04-09 11:36:26 -0400

    The program

---
+++ PasswordChecker.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.*;
+
+/**
+Class name PasswordChecker
+*/
+public class PasswordChecker{
+  public static void main(String[] args){
+    /**
+    calls Min method from PasswordMin class
+    */
+    PasswordMin.Min();
+
+    System.out.println("Congratulations your password is strong!"); // congratulates user
+    System.out.println(" "); // makes it easier to read in the terminal
+  }
+}
---
+++ RevisedPasswordMin.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+import java.io.*;
+
+
+public class PasswordMin{
+
+  public static void Min(){
+
+
+    while(true; i++) {
+
+      i=0
+;
+      boolean number = false;
+
+      boolean upCharacters = false;
+
+      boolean lowCharacters = false;
+
+      boolean specialChar = false;
+
+      Scanner sc=new Scanner(System.in);
+
+
+      System.out.println("Please enter a password... ");
+
+      String password=sc.nextLine();
+
+
+      if(password.length() < 8){
+
+        System.out.println("Your password is not long enough.");
+
+      } else {
+        String passwordArray[] = password.split("");
+
+
+        if(passwordArray[i] >= "A" && passwordArray[i] <= "Z"){
+
+          upCharacters = true;
+
+
+          } else {
+
+            UpperLettersTest = 1;
+            
+
+
+
+
+
+
+
+
+
+          }
+        if(passwordArray[i] >= "a" && passwordArray[i] <= "z"){
+
+          lowCharacters = true;
+
+
+          else{
+
+            System.out.println("You have no lowercase letters.");
+
+        else{
+        System.out.println("Your password is long enough");
+
+          }
+
+        }
+
+      }
+
+    }
+
+}
